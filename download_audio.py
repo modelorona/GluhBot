87b34b2596d65a9f982e38d2b19fe02d3ea831ed
Anguel Hristozov <angel.hristozov@gmail.com>
2020-10-29 20:53:51 +0000

    google drive downloader seems complete (for now)

--- download_audio.py
+++ download_audio.py
@@ -1,7 +1,10 @@
 import googleapiclient.discovery
 import asyncio
 from google.oauth2 import service_account
-from os import getenv, path, listdir
+from googleapiclient.http import MediaIoBaseDownload
+from io import BytesIO
+from os import getenv, path, mkdir
+from set_up_logging import get_logger
 
 
 class GoogleDriveFolderDownloader():
@@ -10,23 +13,42 @@ class GoogleDriveFolderDownloader():
         service_account_file = 'credentials.json'
         credentials = service_account.Credentials.from_service_account_file(
                 service_account_file, scopes=scopes)
-        self.audio = 'test_audio'
+        self.audio = 'audio'
         self.drive = googleapiclient.discovery.build('drive', 'v3', credentials=credentials)
         self.folder_id = getenv('DRIVE_FOLDER_ID')
 
-    def audio_folder_exists(self):
-        return path.exists('test_audio') 
+    def audio_folder_exists(self) -> bool:
+        return path.exists(self.audio)
 
     def download_audio(self):
-        if self.audio_folder_exists:
-            print('audio folder already exists, will not download audio files')
+        if self.audio_folder_exists():
+            get_logger().info('audio folder already exists, will not download audio files')
         else:
-            print('audio folder does not exist, starting file downloads')
+            get_logger().info('audio folder does not exist, starting file downloads')
+            try:
+                mkdir(self.audio)
+            except OSError as e:
+                print(e)
+                return
             self.download_all_audio()
 
     def download_all_audio(self):
         files = self.drive.files().list(q="'{}' in parents".format(self.folder_id)).execute().get('files', [])
         if files is []:
-            print('google drive api returned empty folder, check api or permissions')
+            get_logger().info('google drive api returned empty folder, check api or permissions')
         else:
-            print('google drive files found, downloading')
+            get_logger().info('google drive files found, downloading')
+            # file_n_ids = [(file.get('id', None), file.get('name', None)) for file in files]
+            for file in files:
+                if file.get('id', None) is None or file.get('name', None) is None: continue  # just in case
+                get_logger().info('downloading audio file: ' + file.get('name'))
+                req = self.drive.files().get_media(fileId=file.get('id'))
+                fh = BytesIO()
+                downloader = MediaIoBaseDownload(fh, req)
+                done = False
+                while not done:
+                    _, done = downloader.next_chunk()
+                with open(self.audio + '/' + file.get('name'), 'wb') as audio_file:
+                    audio_file.write(fh.getbuffer())
+                    get_logger().info('downloaded audio file: ' + file.get('name'))
+            
--- set_up_logging.py
+++ set_up_logging.py
@@ -1,9 +1,14 @@
 import logging
 from os import getenv
 
-if getenv("LOGGING"):
+if getenv('LOGGING'):
     logger = logging.getLogger('discord')
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
+
+
+def get_logger():
+    if getenv('LOGGING'):
+        return logging.getLogger('discord')
