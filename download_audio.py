39357ca971c1e21730b5eb1d13a1a3fc5f6d98b5
Anguel Hristozov <angel.hristozov@gmail.com>
2020-11-09 02:47:19 +0000

    untested fix for non mp3 files, but should work for all audio that ffmpeg supports

--- download_audio.py
+++ download_audio.py
@@ -2,8 +2,9 @@ import googleapiclient.discovery
 from google.oauth2 import service_account
 from googleapiclient.http import MediaIoBaseDownload
 from io import BytesIO
-from os import getenv, path, mkdir
+from os import getenv, path, mkdir, remove
 from json import loads
+from pydub import AudioSegment
 from bot_logger import info as log_info, error as log_error
 
 
@@ -39,17 +40,24 @@ class GoogleDriveFolderDownloader():
             log_info('google drive api returned empty folder, check api or permissions')
         else:
             log_info('google drive files found, downloading')
-            # file_n_ids = [(file.get('id', None), file.get('name', None)) for file in files]
             for file in files:
                 if file.get('id', None) is None or file.get('name', None) is None: continue  # just in case
-                log_info('downloading audio file: ' + file.get('name'))
                 req = self.drive.files().get_media(fileId=file.get('id'))
                 fh = BytesIO()
                 downloader = MediaIoBaseDownload(fh, req)
                 done = False
                 while not done:
                     _, done = downloader.next_chunk()
-                with open(self.audio + '/' + file.get('name'), 'wb') as audio_file:
+                    
+                file_name, file_extension = file.get('name').split('.')
+                file_loc = "{}/{}.{}".format(self.audio, file_name, file_extension)
+
+                with open(file_loc, 'wb') as audio_file:
                     audio_file.write(fh.getbuffer())
-                    log_info('downloaded audio file: ' + file.get('name'))
+                    log_info('downloaded audio file: ' + file_loc)
+                    if file_extension != 'mp3':
+                        log_info('converting file to mp3')
+                        wav_file = AudioSegment.from_file(file_loc, file_extension)
+                        wav_file.export("{}/{}.{}".format(self.audio, file_name, 'mp3'), format='mp3', bitrate='320k')
+                        if path.exists(file_loc): remove(file_loc)
             
